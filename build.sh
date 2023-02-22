9b5faa4bda90141c15a2ade30b94adc93af63d4d
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-02-22 01:37:09 -0500

    handle css better

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 npx tsc
 mkdir -p static/js
+cp node_modules/chessboardjs/www/css/chessboard.css static/chessboard.css
 ./node_modules/.bin/browserify src/index.js -o static/js/index.js
