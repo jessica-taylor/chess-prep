7d18d587c90e95f567cc34ea6ff3a5f30cbafed3
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-02-22 00:34:40 -0500

    expand/contract

--- src/index.ts
+++ src/index.ts
@@ -7,13 +7,12 @@ type PrepNode = {
 }
 
 class PrepView {
-  public root: PrepNode = {expanded: false, moves: {}};
+  public root: PrepNode = {expanded: true, moves: {}};
   constructor() {
-    this.root.expanded = true;
     this.root.moves['e4'] = {expanded: false, moves: {}};
     this.root.moves['e5'] = {expanded: true, moves: {'Nf6': {expanded: false, moves: {}}}};
   }
-  render(node: PrepNode): JQuery  {
+  render(node: PrepNode): JQuery {
     let res = $('<div class="prep-node">');
     if (!node.expanded) {
       return res;
@@ -22,11 +21,13 @@ class PrepView {
     for (let move in node.moves) {
       let child = node.moves[move];
       let li = $('<li class="prep-li">');
-      if (child.expanded) {
-        li.append($('<span>').text('\u25BC'));
-      } else {
-        li.append($('<span>').text('\u25B6'));
-      }
+      let expText = child.expanded ? '\u25BC' : '\u25B6';
+      let exp = $('<span class="prep-exp">').text(expText);
+      exp.click(() => {
+        child.expanded = !child.expanded;
+        this.rerender();
+      });
+      li.append(exp);
       li.append($('<span>').text(move));
       li.append(this.render(child));
       ul.append(li);
@@ -34,12 +35,16 @@ class PrepView {
     res.append(ul);
     return res;
   }
+  rerender() {
+    $('#prep-display').empty();
+    $('#prep-display').append(this.render(this.root));
+  }
 }
 
 function main() {
   $(function() {
     let view = new PrepView();
-    $('#prep-display').append(view.render(view.root));
+    view.rerender();
   });
 }
 
