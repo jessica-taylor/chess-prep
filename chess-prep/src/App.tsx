99593fda656d9db9fadf01218a6fcf621311a017
Jessica Taylor <jessi.liu.taylor@gmail.com>
2023-02-22 19:35:22 -0500

    react stuff...

--- chess-prep/src/App.tsx
+++ chess-prep/src/App.tsx
@@ -2,23 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-type MoveProps = {algebraic: string, isWhite: boolean};
-type MoveState = {focused: boolean};
+type MoveProps = {algebraic: string, isWhite: boolean, focused: boolean, recommended: boolean};
+type MoveState = {};
 
 class Move extends React.Component<MoveProps, MoveState> {
   constructor(props: MoveProps) {
     super(props);
-    this.state = {focused: true};
+    this.state = {};
   }
   render() {
     let className = 'prep-move';
-    if (this.state.focused) {
+    if (this.props.focused) {
       className += ' prep-focus';
     }
     if (this.props.isWhite) {
       className += ' prep-white';
     } else {
       className += ' prep-black';
+    }
+    if (this.props.recommended) {
+      className += ' prep-recommended';
     }
     return (
       <span className="Move">
@@ -28,7 +31,31 @@ class Move extends React.Component<MoveProps, MoveState> {
   }
 }
 
-function App() {
+// type NodeProps = {
+//   moves: MoveProps[]
+// };
+// 
+// type NodeState = {
+// };
+// 
+// class Node extends React.Component<NodeProps, NodeState> {
+//   constructor(props: NodeProps) {
+//     super(props);
+//   }
+//   render() {
+//     let lis = [];
+//     for (let move of this.props.moves) {
+//       lis.push(<li><Move {...move} /></li>);
+//     }
+//     return (
+//       <div className="Node">
+//         <ul>{lis}</ul>
+//       </div>
+//     )
+//   }
+// }
+
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -36,7 +63,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Move algebraic="e4" isWhite={true}/>
+        <Move algebraic="e4" isWhite={true} focused={true} recommended={true}/>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -50,4 +77,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
